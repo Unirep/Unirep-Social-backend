0330ea8733b6f0c41d1f42a66d924f3ce2f62cb7
yu-zhen <wlphg010688@gmail.com>
2022-02-08 18:11:07 +0800

    add hardhat provider in constants

--- src/constants.ts
+++ src/constants.ts
@@ -5,6 +5,7 @@ export const DEPLOYER_PRIV_KEY = '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed
 export const UNIREP = '0x0165878A594ca255338adfa4d48449f69242Eb8F';
 export const UNIREP_SOCIAL = '0xa513E6E4b8f2a923D98304ec87F64353C4D5C853';
 export const DEFAULT_ETH_PROVIDER = 'http://localhost:8545';
+// export const DEFAULT_ETH_PROVIDER = 'http://18.188.136.227';
 export const DEFAULT_START_BLOCK = 0;
 export const UNIREP_SOCIAL_ATTESTER_ID = 1
 
