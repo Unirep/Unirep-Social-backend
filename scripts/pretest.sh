e43c6a35d83779b50fbfa0e4cadcc4e1af4aa712
Chance Hudson <jchancehud@gmail.com>
2022-03-03 19:23:42 -0600

    chore: pin docker image versions, supress stop/rm warnings

--- scripts/pretest.sh
+++ scripts/pretest.sh
@@ -5,15 +5,15 @@ GANACHE_NAME=unirep_ganache_test
 TEST_ACCOUNT_1_KEY=0x0000000000000000000000000000000000000000000000000000000000000001
 TEST_ACCOUNT_2_KEY=0x0000000000000000000000000000000000000000000000000000000000000002
 
-docker stop $MONGO_NAME
-docker rm $MONGO_NAME
-docker stop $GANACHE_NAME
-docker rm $GANACHE_NAME
+docker stop $MONGO_NAME 2> /dev/null
+docker rm $MONGO_NAME 2> /dev/null
+docker stop $GANACHE_NAME 2> /dev/null
+docker rm $GANACHE_NAME 2> /dev/null
 
 set -e
 
-docker run -d --name $MONGO_NAME --rm -p 27017:27017 mongo:latest
-docker run -d --name $GANACHE_NAME -p 8545:8545 -p 8546:8546 trufflesuite/ganache-cli:latest \
+docker run -d --name $MONGO_NAME --rm -p 27017:27017 mongo:4.4
+docker run -d --name $GANACHE_NAME -p 8545:8545 -p 8546:8546 trufflesuite/ganache-cli:v6.12.2 \
   --account $TEST_ACCOUNT_1_KEY,10000000000000000000000000000 \
   --account $TEST_ACCOUNT_2_KEY,10000000000000000000000000000 \
   --gasLimit 10000000
