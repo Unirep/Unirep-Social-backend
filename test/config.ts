0b4d018274cae69078d39d59133e82db3995fc7a
Ya-wen, Jeng <vivi432@yahoo.com.tw>
2022-04-06 23:36:49 +0800

    Update circuits (#27)
    * chore: update circuits
    
    * fix: lint

--- test/config.ts
+++ test/config.ts
@@ -1,18 +1,15 @@
+import {
+    circuitEpochTreeDepth,
+    circuitGlobalStateTreeDepth,
+    circuitUserStateTreeDepth,
+    maxReputationBudget,
+    numEpochKeyNoncePerEpoch,
+} from '@unirep/circuits/config'
 import { ethers } from 'ethers'
 
 const attestingFee = ethers.utils.parseEther('0.1')
 
-const numEpochKeyNoncePerEpoch = 3
-
-const numAttestationsPerProof = 5
-
 const epochLength = 900 // seconds
-
-const circuitGlobalStateTreeDepth = 5
-
-const circuitUserStateTreeDepth = 5
-
-const circuitEpochTreeDepth = 32
 
 const globalStateTreeDepth = circuitGlobalStateTreeDepth
 
@@ -20,24 +17,21 @@ const userStateTreeDepth = circuitUserStateTreeDepth
 
 const epochTreeDepth = circuitEpochTreeDepth
 
-const maxReputationBudget = 10
-
 const maxUsers = 2 ** circuitGlobalStateTreeDepth - 1
 
 const maxAttesters = 2 ** circuitUserStateTreeDepth - 1
 
-export default {
+export const settings = {
     attestingFee,
-    circuitGlobalStateTreeDepth,
-    circuitUserStateTreeDepth,
-    circuitEpochTreeDepth,
     epochLength,
-    epochTreeDepth,
-    globalStateTreeDepth,
     numEpochKeyNoncePerEpoch,
-    numAttestationsPerProof,
     maxUsers,
     maxAttesters,
-    userStateTreeDepth,
     maxReputationBudget,
+}
+
+export const treeDepth = {
+    epochTreeDepth,
+    globalStateTreeDepth,
+    userStateTreeDepth,
 }
