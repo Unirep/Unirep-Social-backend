cb24170b81311d73af80302c45389aeda77287cf
Chance Hudson <jchancehud@gmail.com>
2022-05-25 22:22:28 -0500

    feat: custom prover using server keys

---
+++ scripts/loadKeys.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+WORKDIR=$(mktemp -d)
+git clone https://github.com/Unirep/circuits.git $WORKDIR
+cp -r $WORKDIR/build ./build/keys
---
+++ src/daemons/Prover.ts
@@ -0,0 +1,19 @@
+import { Circuit } from '@unirep/circuits'
+import * as snarkjs from 'snarkjs'
+import path from 'path'
+
+export class Prover {
+    private static _default = new Prover()
+    static get default() {
+        return this._default
+    }
+    async verifyProof(type: Circuit, proof, signals: bigint[]) {
+        // we'll handle loading here
+        const basepath = path.join(__dirname, '../../build/keys/', type)
+        // const zkeypath = `${basepath}.zkey`
+        const vkeypath = `${basepath}.vkey.json`
+        // const wasmpath = `${basepath}.wasm`
+        const vkey = require(vkeypath)
+        return snarkjs.groth16.verify(vkey, signals, proof)
+    }
+}
