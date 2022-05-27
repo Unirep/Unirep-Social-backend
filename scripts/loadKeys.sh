8d163ee10b18db53cd4c7627df98c7a36a11632a
Chance Hudson <jchancehud@gmail.com>
2022-05-27 18:25:38 -0500

    fix: load keys in postinstall

--- scripts/loadKeys.sh
+++ scripts/loadKeys.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-mkdir build || true
+mkdir build 2> /dev/null || true
 WORKDIR=$(mktemp -d)
 git clone https://github.com/Unirep/circuits.git $WORKDIR
 cp -r $WORKDIR/build ./build/keys
